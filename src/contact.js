229a6446d1e4dcec2ffe93820f20cfca1aa9acd9
Martha-Iradukunda <marthairadukunda1@gmail.com>
2023-02-16 20:35:41 +0300

    dashboard modified

---
+++ src/contact.js
@@ -0,0 +1,6 @@
+// import Form from "./form";
+const Form = require('./form')
+
+const rootNode = document.getElementById('root');
+const root = ReactDOM.createRoot(rootNode);
+root.render(React.createElement(LikeButton));
---
+++ src/form.js
@@ -0,0 +1,22 @@
+// const Form = () => {
+//     const inputs = ["email",
+//         "fname",
+//         "lname",
+//         "message",
+//         "submit"
+//     ];
+//     return ( <
+//         form > {
+//             inputs.map((element, index) => { <
+//                 input key = "index"
+//                 name = { element }
+//                 placeholder = { element }
+//                 />
+
+//             })
+//         } <
+//         /form>
+//     )
+// }
+
+// export default Form;
