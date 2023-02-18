907bf7323a877b787ee8df5c22a865ee9b8e9ac7
Martha-Iradukunda <marthairadukunda1@gmail.com>
2023-02-19 02:20:23 +0300

    dashboard modified

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,7 @@
 // import Form from "./form";
 const Form = require('./form')
 
-const rootNode = document.getElementById('root');
-const root = ReactDOM.createRoot(rootNode);
-root.render(React.createElement(LikeButton));
+const rootNode = document.getElementById('root');//getting element from the doc
+const root = ReactDOM.createRoot(rootNode);//creating react dom
+
+root.render(<Form/>)//any component needed to be rendered here
